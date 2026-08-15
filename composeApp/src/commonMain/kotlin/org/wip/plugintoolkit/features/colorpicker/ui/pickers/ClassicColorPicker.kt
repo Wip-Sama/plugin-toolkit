a232e9edea402b6b6572ca2cf8ade8625488cdcf
Matteo Mekhail <67237370+matteoiscrying@users.noreply.github.com>
2026-08-15 10:08:22 +1000

    Revert "Merge branch 'codex/issue-13-color-picker-rework' into codex/issue-4-plugin-defined-ui"
    This reverts commit 774280b0b617bb366596c3909fe1f38eeedf3125, reversing
    changes made to c38bb106fd87df0070018505617a0cb25d703c5e.

--- composeApp/src/commonMain/kotlin/org/wip/plugintoolkit/features/colorpicker/ui/pickers/ClassicColorPicker.kt
+++ composeApp/src/commonMain/kotlin/org/wip/plugintoolkit/features/colorpicker/ui/pickers/ClassicColorPicker.kt
@@ -35,8 +35,6 @@ import org.wip.plugintoolkit.features.colorpicker.utils.fromHueProgress
 import org.wip.plugintoolkit.features.colorpicker.utils.green
 import org.wip.plugintoolkit.features.colorpicker.utils.lighten
 import org.wip.plugintoolkit.features.colorpicker.utils.red
-import org.wip.plugintoolkit.features.colorpicker.utils.saturationAndValue
-import org.wip.plugintoolkit.features.colorpicker.utils.toHueProgress
 import kotlin.math.roundToInt
 import org.wip.plugintoolkit.core.theme.ToolkitTheme
 
@@ -47,33 +45,18 @@ import org.wip.plugintoolkit.core.theme.ToolkitTheme
 internal fun ClassicColorPicker(
     modifier: Modifier = Modifier,
     showAlphaBar: Boolean,
-    initialColor: Color = Color.White,
     onPickedColor: (Color) -> Unit
 ) {
-    val initialSaturationAndValue = remember(initialColor) { initialColor.saturationAndValue() }
-    val initialHue = remember(initialColor) { initialColor.toHueProgress() }
-    var pickerLocation by remember(initialColor) { mutableStateOf(Offset.Zero) }
+    var pickerLocation by remember { mutableStateOf(Offset.Zero) }
     var colorPickerSize by remember { mutableStateOf(IntSize.Zero) }
-    var pickerInitialized by remember(initialColor) { mutableStateOf(false) }
-    var alpha by remember(initialColor) { mutableStateOf(initialColor.alpha) }
-    var rangeColor by remember(initialColor) { mutableStateOf(Color.fromHueProgress(initialHue)) }
-    var hueSlider by remember(initialColor) { mutableStateOf(initialHue) }
+    var alpha by remember { mutableStateOf(1f) }
+    var rangeColor by remember { mutableStateOf(Color.White) }
+    var hueSlider by remember { mutableStateOf(0f) }
 
-    var color by remember(initialColor) { mutableStateOf(initialColor) }
+    var color by remember { mutableStateOf(Color.White) }
 
-    LaunchedEffect(colorPickerSize, initialColor) {
-        if (colorPickerSize.width > 0 && colorPickerSize.height > 0 && !pickerInitialized) {
-            val (saturation, value) = initialSaturationAndValue
-            pickerLocation = Offset(
-                x = saturation * colorPickerSize.width,
-                y = (1f - value) * colorPickerSize.height
-            )
-            pickerInitialized = true
-        }
-    }
-
-    LaunchedEffect(rangeColor, pickerLocation, colorPickerSize, alpha, pickerInitialized) {
-        if (pickerInitialized && colorPickerSize.width > 0 && colorPickerSize.height > 0) {
+    LaunchedEffect(rangeColor, pickerLocation, colorPickerSize, alpha) {
+        if (colorPickerSize.width > 0 && colorPickerSize.height > 0) {
             val xProgress = if (colorPickerSize.width > 0) {
                 (1 - (pickerLocation.x / colorPickerSize.width)).coerceIn(0f, 1f)
             } else 0f
@@ -159,3 +142,4 @@ private fun ClassicColorPickerPreview() {
         ClassicColorPicker(showAlphaBar = true, onPickedColor = {})
     }
 }
+
